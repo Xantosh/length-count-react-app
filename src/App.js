6421f16bd26cf47865f72644d5d95ff6cc97c436
Santosh Upreti <69950069+Xantosh@users.noreply.github.com>
2021-11-28 18:25:21 +0545

    changes done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      textarea : '',
+      buttondisabled : true
+    }
+    
+    this.textEventHandle = this.textEventHandle.bind(this);
+  }
+
+  displayForm()
+  {
+    return(
+      <div className = "textArea">
+        <textarea  cols = "80" rows ="25" onChange = {this.textEventHandle}></textarea>
+        <button  disabled = {this.state.buttondisabled} onClick = {this.submitOnClick}>Submit</button>
+      </div>
+
+    );
+  }
+  textEventHandle(event){
+    const textarea = event.target.value
+    this.setState({textarea} , () => {this.buttonDisabled()});  
+  }
+
+  submitOnClick(event){
+    event.preventDefault(); 
+    alert("Sucessfully Submitted");
+  }
+
+  updateLength(){
+    return(
+      <div>{`${this.state.textarea.length} words.`}</div>
+    )
+
+  }
+  buttonDisabled(){
+    if(this.state.buttondisabled){
+      if(this.state.textarea.length < 100){
+        this.setState({buttondisabled : true})
+      }
+      else{
+        this.setState({buttondisabled : false})
+      }
+    }
+    else{
+        if(this.state.textarea.length < 100){
+          this.setState({buttondisabled : true})
+        }
+        else{
+          this.setState({buttondisabled : false})
+        }  
+    }
+  }
+    
+
+
+
+
+
+  render(){
+    return(
+      <div className = "App">
+        <h1>Essay</h1><br/>
+        <p>Plz write atleast 100 words.</p>
+        <hr/>
+
+        {this.displayForm()}
+        {this.updateLength()}
+      </div>
+    );
+  }
 }
 
 export default App;
